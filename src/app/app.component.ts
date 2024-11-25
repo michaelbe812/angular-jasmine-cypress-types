fc692d3a394a3bd9bf8ab386e5ebc92a910a2393
Michael Berger <m.gonser@icloud.com>
2024-11-25 14:25:38 +0100

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LazyComponent} from './lazy/lazy.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [ LazyComponent],
+  styleUrl: './app.component.css',
+  template: `
+    <div></div>
+    @defer (on hover) {
+        <app-lazy/>
+    } @placeholder{
+        <div>Platzhalter</div>
+    }
+
+
+`
 })
 export class AppComponent {
   title = 'lv-modern-angular';
---
+++ src/app/lazy/lazy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy',
+  imports: [],
+  templateUrl: './lazy.component.html',
+  styleUrl: './lazy.component.css'
+})
+export class LazyComponent {
+
+}
